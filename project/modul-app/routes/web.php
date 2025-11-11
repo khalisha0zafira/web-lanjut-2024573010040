6900764c798cd7d1b2ffac48cff4a5c4f80fc6b6
Khalisha <khalishazhafira76@gmail.com>
2025-11-11 14:46:47 +0700

    update folder

---
+++ project/modul-app/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+Route::post('/product/result', [ProductController::class, 'result'])->name('product.result');
+
+Route::get('/', function () {
+    return view('welcome');
+});
---
+++ project/productpagination/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/', function () {
+    return view('welcome');
+});
